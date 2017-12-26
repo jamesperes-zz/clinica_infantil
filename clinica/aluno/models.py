16c67b584cf6b0b1bb45bc5086bf0787dbc29109
Felipe Barros Caminha <felipebarroscaminha@gmail.com>
2017-12-25 22:31:41 -0300

    Updated class Aluno

--- clinica/aluno/models.py
+++ clinica/aluno/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 from padroes import choices
 
 class Aluno(models.Model):
 	#Dados Pessoais
 	nome = models.CharField("Nome", max_length = 30, null = True)
 	cpf = models.CharField("CPF", max_length = 11, null = True)
+	sexo = models.CharField("Sexo", max_length = 1, choices = choices.SEXI_CHOICES)
 	data_nascimento = models.DateField("Data de Nascimento", null = True)
 
 	#Dados Academicos
@@ -12,3 +14,10 @@ class Aluno(models.Model):
 
 	#Contato
 	telefone = models.CharField("Telefone", max_length = 12, null = True)
+	email = models.EmailField(null = True)
+
+	#Status
+	aprovado = models.BooleanField(default=False)
+
+	#User
+	user = models.OneToOneField(User, on_delete = models.CASCADE, null = True)
