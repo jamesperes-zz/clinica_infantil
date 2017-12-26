d59ed4b385b264f99c4ebff9b4add8885756f8cf
Felipe Barros Caminha <felipebarroscaminha@gmail.com>
2017-12-25 22:21:03 -0300

    Updated Aluno

--- clinica/aluno/models.py
+++ clinica/aluno/models.py
@@ -2,4 +2,13 @@ from django.db import models
 from padroes import choices
 
 class Aluno(models.Model):
-	nome = models.CharField("Nome", max_length=30,)
+	#Dados Pessoais
+	nome = models.CharField("Nome", max_length = 30, null = True)
+	cpf = models.CharField("CPF", max_length = 11, null = True)
+	data_nascimento = models.DateField("Data de Nascimento", null = True)
+
+	#Dados Academicos
+	matricula = models.CharField("Matrícula", max_length = 11, null = True)
+
+	#Contato
+	telefone = models.CharField("Telefone", max_length = 12, null = True)
